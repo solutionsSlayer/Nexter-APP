108ceeed20ef479ed4513a4dd08f7105eda72d53
Doussain <ddoussain@gmail.com>
2020-04-16 16:50:46 +0200

    Handle rejections

--- server.js
+++ server.js
@@ -21,3 +21,18 @@ const DB = mongoose.connect(uri, {
 });
 
 app.listen(port);
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
