88d85ce69bdb1767b94f836095eb89036291e786
Doussain <ddoussain@gmail.com>
2020-04-16 22:04:38 +0200

    Make it better

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config({
   path: `${__dirname}/config.env`
 });
 
-const uri = process.env.MONGODB_URI.replace('<password>', process.env.DB_PASSWORD) || 'mongodb://localhost:27017';
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
 const port = process.env.DB_PORT || 3000;
 
 mongoose.connect(uri, {
