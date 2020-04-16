2d5eb56a30ee07104f4b0e0222041f442a5516c3
Doussain <ddoussain@gmail.com>
2020-04-16 20:55:46 +0200

    Make it better

--- server.js
+++ server.js
@@ -13,25 +13,9 @@ const port = process.env.DB_PORT || 3000;
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
-});
-
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-  console.log(err.name, err.message, err);
-  server.close(() => {
-    process.exit(1);
-  });
-});
-
-process.on("SIGTERM", () => {
-  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
-  server.close(() => {
-    console.log("💥 Process terminated!");
-  });
 });
