be388eea4cbebca19502118c1662ce5658242bdd
Doussain <ddoussain@gmail.com>
2020-04-16 21:35:48 +0200

    Make it better

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-/*eslint-disable-next-line*/
 require("dotenv").config({
   path: `${__dirname}/config.env`
 });
 
-const uri = 'mongodb+srv://Dorian:m5UwTj2mgpWtJxRr@cluster0-f3gw3.mongodb.net/test?retryWrites=true'
+const uri = process.env.MONGODB_URI.replace('<password>', process.env.DB_PASSWORD);
 const port = process.env.DB_PORT || 3000;
 
 mongoose.connect(uri, {
